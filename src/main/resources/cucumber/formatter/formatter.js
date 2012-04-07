da38c75f982e375ad060a7b06a47632dc3e3f434
Renier Morales <renierm@us.ibm.com>
2012-04-07 01:30:21 -0400

    requirejs-ify the html formatter without breaking it in node.

--- src/main/resources/cucumber/formatter/formatter.js
+++ src/main/resources/cucumber/formatter/formatter.js
@@ -194,4 +194,6 @@ CucumberHTML.templates = '<div>\
 
 if (typeof module !== 'undefined') {
   module.exports = CucumberHTML;
+} else if (typeof define !== 'undefined') {
+  define([], function() { return CucumberHTML; });
 }
