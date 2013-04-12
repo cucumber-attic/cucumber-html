8b87639a3c0252c81c34b77b033877e75b8936d8
Tom Dunstan <github@tomd.cc>
2013-04-12 10:50:53 +0930

    added before and after blocks to example report.js

--- src/main/resources/cucumber/formatter/report.js
+++ src/main/resources/cucumber/formatter/report.js
@@ -31,6 +31,7 @@ for(var n = 0; n < N; n++) {
   formatter.match({uri:'report.feature'});
   formatter.result({status:'passed', duration: 0});
 
+  formatter.before({status: 'passed', duration: 668816288});
   formatter.scenario({"tags":[{"name":"@foo","line":3},{"name":"@bar","line":4},{"name":"@doh","line":5}], keyword:'Scenario', name: 'Creating a simple report', line: 6});
   formatter.step({keyword:'Given ', name:'I have a feature', line: 7, doc_string:{value: "A\ndoc string\non several lines", content_type:"text/plain", line:8}});
   formatter.step({keyword:'When ', name:'I format it', line: 11});
@@ -48,16 +49,19 @@ for(var n = 0; n < N; n++) {
   formatter.write('What a nice helicopter');
   formatter.match({uri:'report.feature'});
   formatter.result({status:'skipped', duration: 0});
+  formatter.after({status: 'passed', duration: 668816288});
 
   formatter.scenarioOutline({keyword:'Scenario Outline', name: 'Scenario with examples', description:'It should be good to format outlined arguments.', line: 16});
   formatter.step({keyword:'Given ', name:'I have a <name> which costs <price>', line: 17});
   formatter.examples({description:'', name:'Some good examples', keyword:'Examples', line: 18, rows:[{cells:['name', 'price'], line:19}, {cells:['milk', '9'], line:20}, {cells:['bread', '7'], line:21}, {cells:['soap', '5'], line:22}]})
+  formatter.before({status: 'passed', duration: 668816288});
   formatter.match({uri:'report.feature'});
   formatter.result({status:'passed', duration: 0});
   formatter.match({uri:'report.feature'});
   formatter.result({status:'passed', duration: 0});
   formatter.match({uri:'report.feature'});
   formatter.result({status:'failed', error_message:'I didn\'t do it.', duration: 0});
+  formatter.after({status: 'failed', duration: 668816288, "error_message": 'com.example.MyDodgyException: Widget underflow\r\n\tat org.codehaus.groovy.runtime.metaclass.ClosureMetaClass.invokeMethod(ClosureMetaClass.java:264)\r\n\tat com.example.WidgetFurbicator.furbicateWidgets(WidgetFurbicator.java:678)'});
 }
 console.log('Rendered %s features in %s ms', N, new Date().getTime() - start);
 
